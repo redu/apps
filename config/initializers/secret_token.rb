53ed354889830feb568ba4e3af77953532c25293
embs <embs@cin.ufpe.br>
2012-10-08 19:17:40 -0300

    Inicia a aplicação Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReduApps::Application.config.secret_token = 'd7b29dddf42c261461a9231d2b0d2338e96096ea097172770594682b6491dcbc384fe1ed0f2a47537a3fc223cd8e5a8ab56a18e315b19f990973ca0c2f65b3be'
