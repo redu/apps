da31a4a0c0c571ff5bc7cf20f2d60d5577ea99fd
Igor Calabria <igor@redu.com.br>
2012-10-18 10:15:19 -0300

    Listagem de favoritos agora é baseada em UserAppAssociation

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,17 @@
 class FavoritesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @apps = @user.apps.paginate(:page => params[:page])
+    @user_apps_association = @user.user_app_associations.paginate(:page => params[:page])
   end
 
   def create
     user = User.find(params[:user_id])
-    user.apps << [App.find(params[:app_id])]
+    user.apps << App.find(params[:app_id])
     redirect_to :action => "index"
   end
 
   def destroy
-    user_id = params[:user_id]
-    app_id = params[:id]
-    association = UserAppAssociation.find_by_user_id_and_app_id(user_id, app_id)
+    association = UserAppAssociation.find(params[:association_id])
     association.destroy
     redirect_to :action => "index"
   end
