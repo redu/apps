462f3e291bea03e9cc32bd19fa69b3599380cc48
embs <embs@cin.ufpe.br>
2012-10-18 10:06:43 -0300

    Refatora CommentsController#create.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,28 @@ class CommentsController < ApplicationController
     #TODO autor do comentário deve ser o usuário da sessão (buscando por login)
     params[:comment][:author] = User.find_by_login(params[:comment][:author])
     @comment = if params[:comment_id]
-        @app.comments <<
-          Comment.find(params[:comment_id]).answers.create(params[:comment])
-      else
-        @app.comments.create(params[:comment])
-      end
+      create_answer
+    else
+      create_comment
+    end
+
     redirect_to app_path(@app)
   end
 
   def destroy
     Comment.find(params[:id]).destroy
+
     redirect_to app_path(App.find(params[:app_id]))
+  end
+
+  private
+
+  def create_comment
+    @app.comments.create(params[:comment])
+  end
+
+  def create_answer
+    @app.comments <<
+      Comment.find(params[:comment_id]).answers.create(params[:comment])
   end
 end
