73e81b36ab7a1aeb1a28b542ed7b41c3c0fe25f0
embs <embs@cin.ufpe.br>
2012-10-24 15:56:03 -0300

    Lança exceção RecordNotFound ao listar favoritos de usuário quando o usuário não existe.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,13 @@
 class FavoritesController < ApplicationController
   def index
     @user = User.find_by_login(params[:user_id])
+    raise ActiveRecord::RecordNotFound unless @user
     @user_apps_associations = @user.user_app_associations.page(params[:page])
   end
 
   def create
     user = User.find_by_login(params[:user_id])
+    raise ActiveRecord::RecordNotFound unless user
     user.apps << App.find(params[:app_id])
     redirect_to action: "index"
   end
