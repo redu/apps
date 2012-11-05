d28788735da6c3656623a982fb1c0a0d0282de2d
embs <embs@cin.ufpe.br>
2012-11-05 18:00:15 -0300

    Integra filtros na tela de aplicativos favoritos (FavoritesController#index)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,22 +4,31 @@ class FavoritesController < ApplicationController
   helper :formatting
 
   def index
-    @user = User.find_by_login(params[:user_id])
-    raise ActiveRecord::RecordNotFound unless @user
-    @user_apps_associations = @user.user_app_associations.page(params[:page])
-    @favorite_apps_count = @user.apps.count
+    @user = current_user
+    @user_apps_associations = assign_user_app_associations.page(params[:page])
+    @favorite_apps_count = @user.apps.size
+    @favorite_apps_filters = Category.filters_on @user.apps.includes(:categories)
+    @favorite_apps_filters_counter = Category.count_filters_on @favorite_apps_filters
+    @filter = params.fetch(:filter, [])
   end
 
   def create
-    user = User.find_by_login(params[:user_id])
-    raise ActiveRecord::RecordNotFound unless user
-    user.apps << App.find(params[:app_id])
+    current_user.apps << App.find(params[:app_id])
     redirect_to action: "index"
   end
 
   def destroy
-    association = UserAppAssociation.find(params[:association_id])
-    association.destroy
+    UserAppAssociation.find(params[:id]).destroy
     redirect_to action: "index"
+  end
+
+  private
+
+  def assign_user_app_associations
+    if params[:filter]
+      @user.user_app_associations.filter(params[:filter])
+    else
+      @user.user_app_associations.includes(app: [:categories, :comments])
+    end
   end
 end
