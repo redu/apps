9cf962d345145f64ae59cb22d7537746f6cf5fe1
Filipe W. Lima <fwl.ufpe@gmail.com>
2012-11-11 22:26:19 -0300

    Consertado o comportamento das mensagens de "Visualizando as últimas respostas..." e "Mostrar todas as X respostas".

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -8,4 +8,18 @@ module AppsHelper
       ret
     end
   end
+
+  # Retorna 2 se length for maior que 2 ou length se menor.
+  def max_2_answers(length)
+    if length > 2
+      2
+    else
+      length
+    end
+  end
+
+  # Verifica se a resposta da vez é a última.
+  def last_answer?(index, total)
+    index + 1 == total
+  end
 end
