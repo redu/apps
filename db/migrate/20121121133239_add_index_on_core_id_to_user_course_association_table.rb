e82d3bd15425ad6f449dcc97870bab465bff9f76
embs <embs@cin.ufpe.br>
2012-11-21 14:20:23 -0300

    Remove migration que adiciona index na coluna core_id para UserCourseAssociations.

--- db/migrate/20121121133239_add_index_on_core_id_to_user_course_association_table.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexOnCoreIdToUserCourseAssociationTable < ActiveRecord::Migration
-  def change
-    remove_index :user_course_associations, [:user_id, :course_id]
-    add_index :user_course_associations, [:core_id, :user_id, :course_id],
-      name: 'index_user_course_associations_on_core_id_and_user_and_course'
-  end
-end
