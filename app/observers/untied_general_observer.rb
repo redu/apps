c6e2c25fe57204127d8789076213b4f2107fea87
Igor Calabria <igor.calabria@gmail.com>
2012-11-28 09:57:44 -0200

    Remove duplicação de configuração no untied_general_observer. closes #issue-118

--- app/observers/untied_general_observer.rb
+++ app/observers/untied_general_observer.rb
@@ -2,8 +2,17 @@ class UntiedGeneralObserver < UntiedObserverHelper
   # Observa todos os objetos relevantes do redu, faz uso do helper para criação
   # dos modelos
 
-  observe(:user, :environment, :course, :space, :subject,
-    :user_course_association, :user_environment_association, :from => :core)
+  # observe(:user, :environment, :course, :space, :subject,
+  #   :user_course_association, :user_environment_association, :from => :core)
+
+  def initialize
+    super
+
+    elements =   ReduApps::Application.config.untied['model_data'].
+      values.collect {|v| v['name'].underscore.to_sym }
+    self.class.observe(*elements, :from => :core)
+  end
+
   def after_create(payload)
     kind = payload.keys[0]
     self.create_proxy(kind, payload.values[0])
